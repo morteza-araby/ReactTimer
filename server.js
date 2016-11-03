0f9b6fa075eb06b23d7299e4d427dcc2cf71e195
Morteza Araby <morteza.araby@ericsson.com>
2016-11-03 11:42:22 +0100

    update

--- server.js
+++ server.js
@@ -1,27 +1,46 @@
-var express = require('express')
+var express = require('express');
 
 // Create our app
+var app = express();
+const PORT = process.env.PORT || 4080;
 
-module.exports = {
-  app: function () {
-    const app = express()
-    const PORT = process.env.PORT || 4080
-
-    // This will forward all https request to http
-    // Should be adjusted accordingly
-    app.use(function (req, res, next) {
-      if (req.headers['x-forwarded-proto'] === 'https') {
-        res.redirect('http://' + req.hostname + req.url)
-      } else {
-        next()
-      }
-    })
-    app.use(express.static(__dirname + '/public'))
-
-   /* app.listen(PORT, function () {
-      console.log('Express server is up on port ' + PORT)
-    })*/
-    return app
+app.use(function (req, res, next){
+  if (req.headers['x-forwarded-proto'] === 'https') {
+    res.redirect('http://' + req.hostname + req.url);
+  } else {
+    next();
   }
-}
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(PORT, function () {
+  console.log('Express server is up on port ' + PORT);
+});
+
+
+// var express = require('express')
+// // Create our app
+// module.exports = {
+//   app: function () {
+//     const app = express()
+//     const PORT = process.env.PORT || 4080
+
+//     // This will forward all https request to http
+//     // Should be adjusted accordingly
+//     app.use(function (req, res, next) {
+//       if (req.headers['x-forwarded-proto'] === 'https') {
+//         res.redirect('http://' + req.hostname + req.url)
+//       } else {
+//         next()
+//       }
+//     })
+//     app.use(express.static(__dirname + '/public'))
+
+//    /* app.listen(PORT, function () {
+//       console.log('Express server is up on port ' + PORT)
+//     })*/
+//     return app
+//   }
+// }
 
