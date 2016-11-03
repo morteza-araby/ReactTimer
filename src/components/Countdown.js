86ecfbd956e571235b4aede9f915ba795e56039b
Morteza Araby <morteza.araby@ericsson.com>
2016-11-03 14:54:42 +0100

    Added countdown form and test

--- src/components/Countdown.js
+++ src/components/Countdown.js
@@ -1,11 +1,29 @@
 import React from 'react'
 import Clock from 'Components/Clock'
+import CountdownForm from 'Components/CountdownForm'
 
 class Countdown extends React.Component {
-    render(){
+
+    constructor(props, context) {
+        super(props, context)
+
+        this.state = {
+            count: 0
+        }
+        this.handleSetCountdown = this.handleSetCountdown.bind(this)
+    }
+
+    handleSetCountdown(seconds) {
+        this.setState({
+            count: seconds
+        })
+    }
+    render() {
+        var {count} = this.state
         return (
             <div>
-                <Clock totalSeconds={129} />
+                <Clock totalSeconds={count} />
+                <CountdownForm onSetCountdown={this.handleSetCountdown} />
             </div>
         )
     }
