c11e451043eb49cf3976d1ccc2ad7c5cda2cbf74
Morteza Araby <morteza.araby@ericsson.com>
2016-11-03 12:54:01 +0100

    Add navigation basics.

---
+++ src/components/Countdown.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Countdown extends React.Component {
+    render(){
+        return (
+            <div>
+                <p>Countdown</p>
+            </div>
+        )
+    }
+}
+
+export default Countdown
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import { Route, IndexRoute } from 'react-router'
-import Main from "Components/Main"
+import Main from 'Components/Main'
+import Timer from 'Components/Timer'
+import Countdown from 'Components/Countdown'
 
 export default(
     <Route path="/" component={Main}>
-        
+        <IndexRoute component={Timer} />
+        <Route path='countdown' component={Countdown} />        
     </Route>
 )
 
