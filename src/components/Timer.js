d24b9277282d5e8b7650fdf35f28169ee9bd2bae
Morteza <morteza.araby@outlook.com>
2016-11-04 20:18:56 +0100

    Created timer componets and tests

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,10 +1,61 @@
 import React from 'react'
+import Clock from 'Components/Clock'
+import Controls from 'Components/Controls'
 
 class Timer extends React.Component {
-    render(){
+
+    constructor(props, context) {
+        super(props, context)
+
+        this.state = {
+            count: 0,
+            timerStatus: 'paused'
+        }
+
+        this.startTimer = this.startTimer.bind(this)
+        this.handleStatusChange = this.handleStatusChange.bind(this)
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if (this.state.timerStatus !== prevState.timerStatus) {
+            switch (this.state.timerStatus) {
+                case 'started':
+                    this.startTimer()
+                    break
+                case 'stopped':
+                    this.setState({ count: 0, timerStatus: 'stopped' })
+                case 'paused':
+                    clearInterval(this.timer)
+                    this.timer = undefined
+                    break
+            }
+        }
+    }
+    startTimer() {
+        this.timer = setInterval(() => {
+            var newCount = this.state.count + 1
+            this.setState({
+                count: newCount >= 0 ? newCount : 0
+            })
+
+        }, 1000)
+    }
+
+    handleStatusChange(newStatus) {
+        this.setState({ timerStatus: newStatus })
+    }
+    render() {
+        var {count, timerStatus} = this.state
+        var renderControlArea = () => {
+            return <Controls
+                countdownStatus={timerStatus}
+                onStatusChange={this.handleStatusChange} />
+        }
         return (
             <div>
-                <p>Timer</p>
+                <h1 className='page-title'> Timer App</h1>
+                <Clock totalSeconds={count} />
+                {renderControlArea()}
             </div>
         )
     }
